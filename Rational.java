6aa305f77dcd8b34e15b66fc71ef3ae918c348d4
Tatiana <tatum@rambler.ru>
2023-01-29 00:41:02 +0300

    Рациональные числа

---
+++ Rational.java
@@ -0,0 +1,22 @@
+class Rational extends Numbers{
+    double real; 
+
+
+    public Rational(double real) {
+        this.real = real;
+
+    }
+
+    public Rational() {
+//        this(0);
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+}
